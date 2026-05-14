61a49236c2e2e66d6b1579954c6ac2ce8c99bddd
panganroseann862-sketch <panganroseann862@gmail.com>
2026-05-14 22:29:51 +0800

    Update 2026_04_05_144525_create_subjects_table.php

--- database/migrations/2026_04_05_144525_create_subjects_table.php
+++ database/migrations/2026_04_05_144525_create_subjects_table.php
@@ -1,22 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('subjects', function (Blueprint $table) {
-            $table->id();
-            $table->string('subject_code');
-            $table->string('subject_name');
-            $table->string('instructor')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('subjects')) {
+            Schema::create('subjects', function (Blueprint $table) {
+                $table->id();
+                $table->string('subject_code');
+                $table->string('subject_name');
+                $table->string('instructor')->nullable();
+                $table->timestamps();
+            });
+        }
     }
-
     public function down(): void
     {
         Schema::dropIfExists('subjects');
